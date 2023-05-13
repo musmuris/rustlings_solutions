334a04c90b222a92d744bc588d00ec1746b14961
Nigel Morse <nigel.morse@altium.com>
2023-05-14 00:41:32 +0100

    quiz2 and options1

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,7 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
+// I AM NOT DON
 
 pub enum Command {
     Uppercase,
@@ -30,20 +30,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            output.push( match command {
+                Command::Uppercase => string.to_uppercase(),                
+                Command::Trim => string.trim().to_string(),
+                Command::Append(count) => string.to_string() + &"bar".repeat(*count)
+            });
         }
         output
     }
-}
+}   
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
